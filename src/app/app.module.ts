ad7c62ca90e6affb37c8ca91c81e701aec48ac54
ThePr0phet <niklaskoch4@googlemail.com>
2021-02-27 12:05:27 +0100

    Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {MatGridListModule} from '@angular/material/grid-list';
-
-import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
