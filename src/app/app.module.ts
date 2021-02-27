784d00b9f1d73509065f974cd988af92b1dfab38
ThePr0phet <niklaskoch4@googlemail.com>
2021-02-27 11:53:35 +0100

    Search Result

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { SearchResultComponent } from './search-result/search-result.component';
+import { ParameterComponentComponent } from './search-result/parameter-component/parameter-component.component';
+import { ResultComponentComponent } from './search-result/result-component/result-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    SearchResultComponent,
+    ParameterComponentComponent,
+    ResultComponentComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    MatGridListModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/models/searchElement.model.ts
@@ -0,0 +1,19 @@
+export class searchElement {
+  id: number;
+  location: string;
+  date: string;
+  time: string;
+  price: string;
+  pictureSrc: string;
+/**
+  printOut = function() {
+    return "ID: " + this.id.toString() +
+          " Location: " + this.location +
+          " Datum: " + this.date +
+          " Uhrzeit: " + this.time +
+          " Preis: " + this.price +
+          "!";
+  }
+
+*/
+}
