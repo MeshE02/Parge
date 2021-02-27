f2de20bbf44a7e718b8d973d7fa9b2234ecebaa0
ThePr0phet <niklaskoch4@googlemail.com>
2021-02-27 15:03:50 +0100

    Zwischenpush

--- src/app/search-result/result-component/result-component.component.ts
+++ src/app/search-result/result-component/result-component.component.ts
@@ -1,4 +1,4 @@
-import { searchElement } from './../../models/searchElement.model';
+import { Parkingplace } from '../../models/Parkingplace.model';
 import { Component, Input, OnInit } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 
@@ -12,22 +12,9 @@ export class ResultComponentComponent implements OnInit {
 
   constructor() { }
 
-  @Input() elements: Array<searchElement>;
+  @Input() elements: Array<Parkingplace>;
 
   ngOnInit(): void {
-    this.tiles = [
-      {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-      {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-      {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-      {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-    ];
 
-    this.elements = [
-      {id: 1, location: "Mannheim", date: "2.3.2021", time: "10:00 - 20:00", price: "3€ pro Stunde", pictureSrc: "example.png"},
-      {id: 2, location: "Heidenheim", date: "3.3.2021", time: "13:00 - 20:00", price: "2€ pro Stunde", pictureSrc: "example.png"},
-      {id: 3, location: "Mannheim-Neckarau", date: "4.3.2021", time: "11:00 - 19:00", price: "4€ pro Stunde", pictureSrc: "example.png"},
-      {id: 4, location: "Mannheim", date: "5.3.2021", time: "08:00 - 22:00", price: "5€ pro Stunde", pictureSrc: "example.png"},
-      {id: 5, location: "Mannheim", date: "6.3.2021", time: "10:00 - 20:00", price: "6€ pro Stunde", pictureSrc: "example.png"},
-    ]
   }
 }
